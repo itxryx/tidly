7dc94074c6df5ce0f3d389e516be80b0a0a67809
ito ryo <electrooculogram@gmail.com>
2025-05-08 01:31:42 +0900

    fix backend authentication

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,9 @@
 import { Hono } from 'hono'
+import { apiAuthMiddleware } from './middlewares/api-auth-middleware'
 
 const app = new Hono()
+
+app.use('/', apiAuthMiddleware)
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
