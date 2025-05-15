c4ea9479badfaeb52c0e8cec00e5f6a942e585aa
ito ryo <electrooculogram@gmail.com>
2025-05-16 03:06:11 +0900

    fix vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,9 @@ import { cloudflare } from "@cloudflare/vite-plugin"
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  server: {
+    host: true,
+  },
   plugins: [
     cloudflare(),
     react(),
